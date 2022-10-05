1dac439301ddd4bc4844bccca1f935c5cb3c38a6
Francisco Bignotto <franciscogrynberg34@gmail.com>
2022-10-05 17:35:44 -0300

    v0.0.48

--- src/components/QuemSomos.js
+++ src/components/QuemSomos.js
@@ -8,27 +8,28 @@ function QuemSomos() {
   return (
     <div id="quemsomos" className="quemsomos" style={{backgroundImage: `url("/fundo-quemsomos.png")` }}>
       <Container fluid>
-        <h1 className="quemsomos__title">Quem são</h1>
+        <h1 className="quemsomos__title title__mobile">Quem são</h1>
         
         <Row>
           <Col xs={{span: 12, order: 2 }} md={{span: 7, order: 1 }}>
-              <p className="quemsomos__text">
-                A Lüchs é diferente de todas as agências que você conhece. 
-                <br></br><br></br>
-                Criamos um método único de comunicação capaz de 
-                aumentar a qualidade da entrega e tornar os 
-                processos claros e agradáveis. 
-                <br></br><br></br>
-                É o mundo do marketing sendo apresentado de 
-                forma mais empática, consciente e cuidadosa.
-                <br></br><br></br>
-                Acreditamos que sua empresa é o seu sonho e torná-lo 
-                um sucesso é o que nos move.
-                <br></br><br></br>
-                Nossa equipe pode ajudar você a desenvolver seu negócio 
-                utilizando as principais ferramentas do marketing digital, 
-                fale conosco e descubra como.
-              </p>
+            <h1 className="quemsomos__title title__desktop">Quem são</h1>
+            <p className="quemsomos__text">
+              A Lüchs é diferente de todas as agências que você conhece. 
+              <br></br><br></br>
+              Criamos um método único de comunicação capaz de 
+              aumentar a qualidade da entrega e tornar os 
+              processos claros e agradáveis. 
+              <br></br><br></br>
+              É o mundo do marketing sendo apresentado de 
+              forma mais empática, consciente e cuidadosa.
+              <br></br><br></br>
+              Acreditamos que sua empresa é o seu sonho e torná-lo 
+              um sucesso é o que nos move.
+              <br></br><br></br>
+              Nossa equipe pode ajudar você a desenvolver seu negócio 
+              utilizando as principais ferramentas do marketing digital, 
+              fale conosco e descubra como.
+            </p>
           </Col>
 
           <Col xs={{span: 12, order: 1 }} md={{span: 5, order: 2 }}>
